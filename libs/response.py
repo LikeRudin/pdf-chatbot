72a18c487f2f3bc3d96b5c8d8fabfec080e0354f
likeRudin <likeRudin@gmail.com>
2024-11-06 09:40:31 +0900

    bugfix: add missing values

--- libs/response.py
+++ libs/response.py
@@ -3,7 +3,7 @@ from typing import Dict, Any, Optional
 
 @dataclass
 class ResponseDict(dict):
-     def __init__(self, success: bool, message: str, data: Dict[str, Any], errors: Optional[Dict[str, Any]] = None):
+     def __init__(self, success: bool, message: str, data: Dict[str, Any] = None, errors: Optional[Dict[str, Any]] = None):
         super().__init__(success=success, message=message, data=data, errors=errors)
 
     
